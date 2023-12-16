4efb43315fcd38e596770b9cba5989ae83cdb02e
Andreas <afdia@users.noreply.github.com>
2023-12-16 12:47:06 +0100

    smaller resized images

--- image_resize.sh
+++ image_resize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd images; for f in ./*.webp ; do convert "$f" -resize 450 -quality 80 -gravity Center -extent 450x220 "../images_resized/${f%.webp}.webp" ; done ; cd ..
+cd images; for f in ./*.webp ; do convert "$f" -resize 450 -gravity Center -extent 450x220 "../images_resized/${f%.webp}.webp" ; done ; cd ..
