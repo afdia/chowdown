3c91474b90ef4ec1ce600a9beb14a8051d30a6ce
Andreas <afdia@users.noreply.github.com>
2024-06-06 19:55:01 +0200

    updated convert to magick

--- image_resize.sh
+++ image_resize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd images; for f in ./*.webp ; do convert "$f" -resize 450 -gravity Center -extent 450x220 "../images_resized/${f%.webp}.webp.avif" ; done ; cd ..
+cd images; for f in ./*.webp ; do magick "$f" -resize 450 -gravity Center -extent 450x220 "../images_resized/${f%.webp}.webp.avif" ; done ; cd ..
